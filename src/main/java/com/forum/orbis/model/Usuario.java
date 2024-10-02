4b202e18021ceca26144876d41916233e9d1d366
adeliadev <arodriguesthamyres@gmail.com>
2024-10-02 11:54:25 -0300

    feat: Adiciona classe Usuario

---
+++ src/main/java/com/forum/orbis/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.forum.orbis.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeDeUsuario;
+
+    private String senha;
+
+    private String email;
+
+    private int nivel;
+}
