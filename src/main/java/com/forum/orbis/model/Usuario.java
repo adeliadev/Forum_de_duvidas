42054f865b819bfb5a8a83d03d733247185c8637
adeliadev <arodriguesthamyres@gmail.com>
2024-10-17 11:48:20 -0300

    chore: Adiciona novas anotações para persistência de Usuario

--- src/main/java/com/forum/orbis/model/Usuario.java
+++ src/main/java/com/forum/orbis/model/Usuario.java
@@ -1,9 +1,11 @@
 package com.forum.orbis.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.List;
 
@@ -16,14 +18,25 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    @NotNull
+    @Size(min = 3, max = 30)
     private String nomeDeUsuario;
 
+    @Column(nullable = false)
+    @NotNull
+    @Size(min = 8)
     private String senha;
 
+    @Column(nullable = false, unique = true)
+    @NotNull
+    @Email
     private String email;
 
-    private int nivel;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Nivel nivel;
 
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Post> posts;
 }
