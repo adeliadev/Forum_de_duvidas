fe513858dee4854d5c0e7f891ae180e1e5a2e3cd
adeliadev <arodriguesthamyres@gmail.com>
2024-10-27 09:12:15 -0300

    chore: Adiciona @Repository nas interfaces repository

--- src/main/java/com/forum/orbis/repository/TopicoRepository.java
+++ src/main/java/com/forum/orbis/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package com.forum.orbis.repository;
 
 import com.forum.orbis.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 }
